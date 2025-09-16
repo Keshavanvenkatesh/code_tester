85328a91ceee2bb1fb6c4d49e3ae913c662a6f76
Keshavan Venkatesh <keshavanvenkatesh7@gmail.com>
2025-09-16 06:24:28 +0530

    Initial commit for code_tester project

--- code_tester.py
+++ code_tester.py
@@ -1,230 +1,185 @@
-#######################################################################################################################################
-# def decorator(func):
-#     def wrapper(*args,**kwargs):
-#         print("before func")
-#         func(*args,**kwargs)
-#         print("after func")
-#     return wrapper
-
-# @decorator
-# def main(person):
-#     print(f"hello {person}")
-# main("keshavan")
-######################################################################################################################################
-
 import random
 import string
 import time
-from datetime import datetime # so you can use time.time()
-import matplotlib.pyplot as plt # for plotting
-import tkinter as tk            # standard shorthand for tkinter
-from klogging import *          # your custom logger
+from datetime import datetime
+import matplotlib.pyplot as plt
+import tkinter as tk
+from klogging import *   # custom logger
 
-def checktype(data):                # this will check the types of the given data and it goes something like this working together with generate specific testcase
-                                    #   data=[1,"hi",[2,"sanjana"]] -> [23213,"hello",[1323,"keshavan"]] this is later deferesensed for the function to use and benchmark perfomance
-        if isinstance(data,bool):
-            return("bool")
-        
-        elif isinstance(data,int):
-            return "int"
-        
-        elif isinstance(data,float):
-            return("float")
-        
-        elif isinstance(data,str):
-            return("str")
-             
-        elif isinstance(data,list):
-            temp=[]
-            for i in data:
-                temp.append(checktype(i))
-            return temp
-        
-        elif isinstance(data,tuple):
-            temp=[]
-            for i in data:
-                temp.append(checktype(i))
-            return tuple(temp)
-        
-        elif isinstance(data,dict):
-            return {k: checktype(v) for k,v in data.items()}
+###################################################################################################
+# TYPE CHECKING + TESTCASE GENERATION
+###################################################################################################
 
-def genereate_spefic_testcase_element(n,i):
+def checktype(data):
+    """Recursively determine the type structure of given data."""
+    if isinstance(data, bool):
+        return "bool"
+    elif isinstance(data, int):
+        return "int"
+    elif isinstance(data, float):
+        return "float"
+    elif isinstance(data, str):
+        return "str"
+    elif isinstance(data, list):
+        return [checktype(i) for i in data]
+    elif isinstance(data, tuple):
+        return tuple(checktype(i) for i in data)
+    elif isinstance(data, dict):
+        return {k: checktype(v) for k, v in data.items()}
 
-    if i=="bool":
-        return (bool(random.randint(0,1)))
-    
-    elif i=="int":
-        return (random.randint(int("0"+("0"*(n-1))),int("9"*n)))
-    
-    elif i=="float":
-        return random.uniform(10**(n-1), 10**n)
 
+def genereate_spefic_testcase_element(n, i):
+    """Generate one testcase element with fixed digit/length constraints."""
+    if i == "bool":
+        return bool(random.randint(0, 1))
+    elif i == "int":
+        return random.randint(int("1" + ("0" * (n - 1))), int("9" * n))
+    elif i == "float":
+        return random.uniform(10 ** (n - 1), 10 ** n)
     elif i == "str":
-        # check for special char and num and generate specific characters
-        temp_str=""
-        for i in range(n):
-            temp_str+=random.choice(list(string.ascii_letters))
-        return temp_str
-    
-    elif isinstance(i,list):
-        temp=[]
-        for j in i:
-            temp.append(genereate_spefic_testcase_element(n,j))
-        return temp
-    
-    elif isinstance(i,tuple):
-        temp=[]
-        for j in i:
-            temp.append(genereate_spefic_testcase_element(n,j))
-        return tuple(temp)
-    
-    elif isinstance(i,dict):
-        return {k: genereate_spefic_testcase_element(n,v) for k,v in i.items()}
+        return "".join(random.choice(string.ascii_letters) for _ in range(n))
+    elif isinstance(i, list):
+        return [genereate_spefic_testcase_element(n, j) for j in i]
+    elif isinstance(i, tuple):
+        return tuple(genereate_spefic_testcase_element(n, j) for j in i)
+    elif isinstance(i, dict):
+        return {k: genereate_spefic_testcase_element(n, v) for k, v in i.items()}
 
-def generate_specific_testcase(example,range_):
-    test_casetype=[]
-    test_case=[]
-    for i in example:
-        test_casetype.append(checktype(i))
 
-    for i in range(len(test_casetype)):
-        test_case.append(genereate_spefic_testcase_element(range_[i],test_casetype[i]))
-    return(tuple(test_case))
+def generate_specific_testcase(example, range_):
+    """Generate a testcase based on exact digit/length constraints."""
+    test_casetype = [checktype(i) for i in example]
+    test_case = [genereate_spefic_testcase_element(range_[i], test_casetype[i]) for i in range(len(test_casetype))]
+    return tuple(test_case)
 
-def genereate_random_testcase_element(i,range_):
 
-    if i=="bool":
-        return (bool(random.randint(0,1)))
-    
-    elif i=="int":
-        digits=random.randint(*range_)                    
-        return (random.randint(10**(digits-1),10**digits-1))
-    
-    elif i=="float":
-        digits=random.randint(*range_)
-        return random.uniform(10**(digits-1),10**digits)
-    
-    elif i=="str":
-        length=random.randint(*range_)
-        temp_str=""
-        for _ in range(length):
-            temp_str+=random.choice(list(string.ascii_letters))
-        return temp_str
-     
-    elif isinstance(i,list):
-        temp=[]
-        for j,sub_range in zip(i,range_):           
-            temp.append(genereate_random_testcase_element(j,sub_range))
-        return temp
-    
-    elif isinstance(i,tuple):
-        temp=[]
-        for j,sub_range in zip(i,range_):
-            temp.append(genereate_random_testcase_element(j,sub_range))
-        return tuple(temp)
-    
-    elif isinstance(i,dict):
-        return {k:genereate_random_testcase_element(v,range_[k]) for k,v in i.items()}
+def genereate_random_testcase_element(i, range_):
+    """Generate one testcase element within a random range."""
+    if i == "bool":
+        return bool(random.randint(0, 1))
+    elif i == "int":
+        digits = random.randint(*range_)
+        return random.randint(10 ** (digits - 1), 10 ** digits - 1)
+    elif i == "float":
+        digits = random.randint(*range_)
+        return random.uniform(10 ** (digits - 1), 10 ** digits)
+    elif i == "str":
+        length = random.randint(*range_)
+        return "".join(random.choice(string.ascii_letters) for _ in range(length))
+    elif isinstance(i, list):
+        return [genereate_random_testcase_element(j, sub_range) for j, sub_range in zip(i, range_)]
+    elif isinstance(i, tuple):
+        return tuple(genereate_random_testcase_element(j, sub_range) for j, sub_range in zip(i, range_))
+    elif isinstance(i, dict):
+        return {k: genereate_random_testcase_element(v, range_[k]) for k, v in i.items()}
 
-def generate_random_testcase(example,range_):
-    test_casetype=[]
-    test_case=[]
 
-    for i in example:
-        test_casetype.append(checktype(i))
+def generate_random_testcase(example, range_):
+    """Generate a testcase with randomized digit/length constraints."""
+    test_casetype = [checktype(i) for i in example]
+    test_case = [genereate_random_testcase_element(test_casetype[i], range_[i]) for i in range(len(test_casetype))]
+    return tuple(test_case)
 
-    for i in range(len(test_casetype)):
-        test_case.append(genereate_random_testcase_element(test_casetype[i],range_[i]))
-    return(tuple(test_case))
 
-#####################################################################################################################
+###################################################################################################
+# BENCHMARKING + LOGGING
+###################################################################################################
 
-def average_time(func,example,range_,iterations=10,type="random",file_to_log="benchmark_logging.txt", max_size = 1048576):
+def average_time(func, example, range_, iterations=10, type="random",
+                 file_to_log="benchmark_logging.txt", max_size=1048576):
+    """
+    Benchmark execution time of a function across multiple runs.
 
-    record_time=[]
-    if type=="specific":
+    Logs details using custom klogging:
+    - Function name
+    - Arguments
+    - Return value
+    - Execution time
+    - Success / Error
+    """
+    record_time = []
 
-        benchmark_logger = logger("benchmark_logger",max_size=1073741824, file_to_log=file_to_log)
+    benchmark_logger = logger("benchmark_logger", max_size=1073741824, file_to_log=file_to_log)
 
-        for i in range(iterations):
-            test_case=generate_specific_testcase(example,range_)
-            return_val=None
+    for i in range(iterations):
+        test_case = (generate_specific_testcase(example, range_) if type == "specific"
+                     else generate_random_testcase(example, range_))
 
-            try:
-                start_time=time.time()
-                return_val=func(*test_case)
-                end_time=time.time()
+        return_val = None
+        try:
+            start_time = time.time()
+            return_val = func(*test_case)
+            end_time = time.time()
 
-                excution_time=float(end_time-start_time)
-                record_time.append(excution_time)
+            exec_time = float(end_time - start_time)
+            record_time.append(exec_time)
 
-                benchmark_logger.log_info(msg="executed successfully",
-                                          PREFIX=str(i+1)+".",
-                                          POSTFIX=f" >>>time of excution({excution_time})",
-                                          file_to_log="benchmark_logging.txt",
-                                          function_name=func.__name__,
-                                          arguments=str(test_case),
-                                          return_value=return_val)
-            except:
-                benchmark_logger.log_error(msg="execution failed",
-                                           PREFIX=str(i+1)+".",
-                                           POSTFIX=" >>>time of excution(none)",
-                                           file_to_log="benchmark_logging.txt",
-                                          function_name=func.__name__,
-                                          arguments=str(test_case),
-                                          return_value=return_val)
+            benchmark_logger.log_info(
+                msg="executed successfully",
+                PREFIX=f"{i+1}.",
+                POSTFIX=f" >>> execution_time({exec_time})",
+                file_to_log=file_to_log,
+                function_name=func.__name__,
+                arguments=str(test_case),
+                return_value=return_val
+            )
 
-        return sum(record_time)/len(record_time)
+        except Exception:
+            benchmark_logger.log_error(
+                msg="execution failed",
+                PREFIX=f"{i+1}.",
+                POSTFIX=" >>> execution_time(none)",
+                file_to_log=file_to_log,
+                function_name=func.__name__,
+                arguments=str(test_case),
+                return_value=return_val
+            )
 
-    elif type=="random":
-        benchmark_logger = logger("benchmark_logger",max_size=1073741824, file_to_log=file_to_log)
+    return sum(record_time) / len(record_time) if record_time else None
 
-        for i in range(iterations):
-            test_case=generate_random_testcase(example,range_)      # range is a nested list of range of each element
 
-            try:
-                start_time=time.time()
-                return_val=func(*test_case)
-                end_time=time.time()
+###################################################################################################
+# ERROR REVIEW + HELP
+###################################################################################################
 
-                excution_time=float(end_time-start_time)
-                record_time.append(excution_time)
-
-                benchmark_logger.log_info(msg="executed successfully",
-                                          PREFIX=str(i+1)+".",
-                                          POSTFIX=f" >>>time of excution({excution_time})",
-                                          file_to_log="benchmark_logging.txt",
-                                          function_name=func.__name__,
-                                          arguments=str(test_case),
-                                          return_value=return_val)
-                
-            except:
-                benchmark_logger.log_error(msg="execution failed",
-                                           PREFIX=str(i+1)+".",
-                                           POSTFIX=" >>>time of excution(none)",
-                                           file_to_log="benchmark_logging.txt",
-                                          function_name=func.__name__,
-                                          arguments=str(test_case),
-                                          return_value=return_val)
-                
 def show_error(write=False):
+    """Show all logged errors from benchmark log."""
     if write:
+        # Future: option to export error logs to a file
         pass
     else:
         logger.show_all_error(log_file="benchmark_logging.txt")
 
-###############################################################################################
 
-# example_testcase=["hi",1,["a",1]]
-# print(generate_specific_testcase(example_testcase))
+def help():
+    """Display usage guide for testcase generator + benchmarking tool."""
+    print(r"""
+🔹 Benchmarking + Testcase Generator Help 🔹
+Provides tools to:
+- Generate random/specific testcases
+- Benchmark execution time
+- Log execution details
+- Review error history
 
-# def func(a,b,c=[1,2]):
-#     print(a,b,c)
-# func(*generate_specific_testcase([True,"hi",[1,2]]))
+📌 Main Functions:
+  checktype(data) → infer type structure
+  generate_specific_testcase(example, range_)
+  generate_random_testcase(example, range_)
+  average_time(func, example, range_, iterations, type, file_to_log, max_size)
+  show_error(write=False)
 
-def func(a,b,c=[1,2]):
-    print(a/b)
+👉 Call help() anytime to show this guide.
+""")
 
-print(average_time(func,[1,2,[1,2]],[1,1,3,3],100,"specific"))
-show_error()
+
+###################################################################################################
+# SAMPLE USAGE
+###################################################################################################
+
+def func(a, b, c=[1, 2]):
+    print(a / b)
+
+
+if __name__ == "__main__":
+    print(average_time(func, [1, 2, [1, 2]], [1, 1, 3, 3], 10, "specific"))
+    show_error()
